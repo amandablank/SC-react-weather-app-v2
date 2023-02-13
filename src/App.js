65d037b5fc1571f446a80a923b4b92781c7db9a2
Amanda <amanda.blankenburg@gmail.com>
2023-02-13 14:42:49 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/amanda-cristina-blankenburg-81b410213/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Amanda Cristina Blankenburg
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/amandablank/weather-app-react-v2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/amanda-cristina-blankenburg-81b410213/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amanda Cristina Blankenburg
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/amandablank/weather-app-react-v2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
