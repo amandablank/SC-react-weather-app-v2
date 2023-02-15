86b39fe71aaf168c41ba7dad5794d33e23aba03d
Amanda <amanda.blankenburg@gmail.com>
2023-02-15 16:28:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Paris" />
+        <Weather defaultCity="Blumenau" />
         <footer className="text-center">
           This project was coded by{" "}
           <a
