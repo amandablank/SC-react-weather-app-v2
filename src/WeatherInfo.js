4fd3ec00fdf655dfa4f184285457e331878495b8
Amanda <amanda.blankenburg@gmail.com>
2023-02-17 17:39:14 +0100

    Added Math.round (wind)

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
         <div className="col-6 weather-description">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
           </ul>
         </div>
       </div>
